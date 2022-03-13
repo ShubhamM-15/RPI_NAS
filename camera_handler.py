8fee4f4d61bfd8e718f4ce65899c69f2b9cfddd0
ShubhamM-15 <shubham.mathur15@gmail.com>
2022-03-13 13:57:02 +0530

    separate camera handler thread introduces lag in frames. Removed.

--- camera_handler.py
+++ camera_handler.py
@@ -21,41 +21,10 @@ class CameraHandler:
 
         self.maxMisses = 10
         self.storage = None
-
-        self.q = Queue(maxsize=1)
         self.cam = None
-        self.camStatus = False
-        self.executor = ThreadPoolExecutor(max_workers=1, thread_name_prefix='camera_daemon')
-
-    def __frameCatchDaemon(self):
-        logger.info("camera_daemon: Running loop for grab retrieve and dump...")
-        errorCount = 0
-        while True:
-            try:
-                ret, fetchFrame = self.cam.read()
-                if ret:
-                    if self.resize:
-                        cv2.resize(fetchFrame, dsize=self.resolution, dst=fetchFrame)
-                    errorCount = 0
-                    try:
-                        self.q.put(fetchFrame, block=False)
-                    except:
-                        pass
-                else:
-                    errorCount += 1
-                    logger.error("camera_daemon: Camera closed while application was running")
-            except Exception as e:
-                errorCount += 1
-                logger.error(f"camera_daemon: Capturing failed: {e}")
-            if errorCount > self.maxMisses:
-                logger.fatal("camera_daemon: Max Misses surpassed. exiting application")
-                self.camStatus = False
-                return False
 
     def __setupCam(self):
         self.cam.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-        #self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.resolution[0])
-        #self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.resolution[1])
 
     def begin(self):
         # This is major camera loop that captures and saves
@@ -89,19 +58,11 @@ class CameraHandler:
             logger.error("Unable to capture frame from camera. exiting")
             return False
 
-        # Launch camera daemon
-        self.executor.submit(self.__frameCatchDaemon)
-        self.camStatus = True
-
         errorCount = 0
-        while self.camStatus:
-            try:
-                fetchFrame = self.q.get(block=True, timeout=1)
-            except:
-                logger.error("unable to dequeue frame from camera_daemon, timed out")
-                errorCount += 1
-                fetchFrame = None
-            if fetchFrame is not None:
+        logger.info("Running camera loop ...")
+        while True:
+            ret, fetchFrame = self.cam.read()
+            if ret:
                 if self.resize:
                     cv2.resize(fetchFrame, dsize=self.resolution, dst=fetchFrame)
                 updated = self.storage.updateFrame(fetchFrame)
@@ -111,13 +72,13 @@ class CameraHandler:
                     errorCount += 1
                 del fetchFrame
                 gc.collect()
+            else:
+                errorCount += 1
+                logger.error("Unable to fetch frame from camera")
             if errorCount >= self.maxMisses:
                 logging.fatal("Camera loop: max error count surpassed. Breaking")
-                self.camStatus = False
                 break
 
         # Force dump
         self.storage.forceDump()
-        self.executor.shutdown(wait=True)
-        logger.info("Exiting camera handler")
 
