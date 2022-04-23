4fb252f25a0b6fdbce14c094ff9a186b88233cbf
ShubhamM-15 <shubham.mathur15@gmail.com>
2022-04-23 10:02:07 +0530

    app restart should be handled from a shell script only.

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ logging.basicConfig(
 logger = logging.getLogger('LOGGER_NAME')
 
 def main():
+    '''
+    Its infinite loop should be handled by the shell script calling this python script
+    :return: It only returns if application has crashed.
+    '''
     # Reading Config
     configPath = os.path.join(os.path.dirname(__file__), 'utils/config.json')
     with open(configPath, "r") as fp:
@@ -29,23 +33,22 @@ def main():
         logger.error("Failed to read config.json")
         return -1
 
-    while True:
-        logger.info("Starting Application")
-        # Finding ip of camera to prepare its stream string
-        deviceHandler = DeviceFinder()
-        ip = deviceHandler.getIPofDevice(config['camera_mac'])
-        if ip is None:
-            logger.fatal(f"Unable to find ip of device with mac {config['camera_mac']}")
-            logger.fatal("Exiting Python Application")
-            return -1
-        stream = config['rstp_string'].format(ip=ip)
-        logger.info(f"Setting camera stream {stream}")
+    logger.info("Starting Application")
+    # Finding ip of camera to prepare its stream string
+    deviceHandler = DeviceFinder()
+    ip = deviceHandler.getIPofDevice(config['camera_mac'])
+    if ip is None:
+        logger.fatal(f"Unable to find ip of device with mac {config['camera_mac']}")
+        logger.fatal("Exiting Python Application")
+        return -1
+    stream = config['rstp_string'].format(ip=ip)
+    logger.info(f"Setting camera stream {stream}")
 
-        # Launching camera handler
-        camHandle = CameraHandler(stream, config)
-        camHandle.begin()
-        logger.error("Camera application crashed. Restarting")
-
+    # Launching camera handler
+    camHandle = CameraHandler(stream, config)
+    camHandle.begin()
+    logger.error("Camera application crashed. Restarting")
+    return -1
 
 if __name__ == "__main__":
     main()
